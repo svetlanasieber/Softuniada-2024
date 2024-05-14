682877642f8ea541ce920931d3c2b96a32b0d0cd
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-14 21:00:02 +0200

    Update 01.Peak Element.cpp

--- 01.Peak Element.cpp
+++ 01.Peak Element.cpp
@@ -4,24 +4,23 @@
 
 int main() {
     std::string line;
-    std::getline(std::cin, line);  // Read a line of input
+    std::getline(std::cin, line); 
     std::istringstream iss(line);
     std::vector<int> numbers;
     int num;
 
-    // Read integers from the input line
+ 
     while (iss >> num) {
         numbers.push_back(num);
     }
 
     int largest_peak = -1;
 
-    // Check for a peak element
     for (int i = 0; i < numbers.size(); ++i) {
-        if ((i == 0 || numbers[i] > numbers[i - 1]) &&  // First element or greater than the previous
-            (i == numbers.size() - 1 || numbers[i] > numbers[i + 1])) {  // Last element or greater than the next
+        if ((i == 0 || numbers[i] > numbers[i - 1]) && 
+            (i == numbers.size() - 1 || numbers[i] > numbers[i + 1])) { 
             if (numbers[i] > largest_peak) {
-                largest_peak = numbers[i];  // Update the largest peak found
+                largest_peak = numbers[i];  
             }
         }
     }
